abf659435c6f1a33a7319f64ed58c0ce28c9447d
agustian2tie <agustian24ti@mahasiswa.pcr.ac.id>
2026-04-08 09:53:32 +0700

    Pertemuan 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import TailwindCSS from '@TailwindCSS/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    TailwindCSS()
+  ]
 })
